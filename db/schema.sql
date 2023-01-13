8761b3d0253fb946357224f261a8dccff18a461c
Siera Ford <sieraford@Sieras-MacBook-Pro.local>
2023-01-12 20:35:35 -0800

    add logic for view employees, add a department and add role

--- db/schema.sql
+++ db/schema.sql
@@ -22,7 +22,6 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
-  manager_id INT,
-  FOREIGN KEY (role_id) REFERENCES role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+  manager_id INT REFERENCES employee(id),
+  FOREIGN KEY (role_id) REFERENCES role(id)
 )
--- index.js
+++ index.js
@@ -25,49 +25,103 @@ const question = [
       "Add a department", 
       "Add a role", 
       "Add an employee", 
-      "Update an employee role"
+      "Update an employee role",
+      "Quit"
     ]
   }
 ];
+
+function promptUser() {
+  inquirer
+  .prompt(question)
+  .then((answers) => {
+    checkMenuSelection(answers)
+  });
+}
 
 function checkMenuSelection(answers) {
   if(answers.menu == "View all departments") {
     db.query('SELECT * FROM department', function (err, results) {
       console.table(results);
+      promptUser();
     });  
   } else if(answers.menu == "View all roles") {
     db.query('SELECT * FROM role', function (err, results) {
       console.table(results);
+      promptUser();
     });
   }  else if(answers.menu == "View all employees") {
-    // presented with a formatted table showing employee data, including employee ids, 
-    // first names, last names, job titles, departments, salaries, and managers that 
-    // the employees report to 
-    db.query('SELECT * FROM employee JOIN role ON employee.role_id = role.id JOIN;', function (err, results) {
+    db.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id;', function (err, results) {
       console.table(results);
+      promptUser();
     });
   } else if(answers.menu == "Add a department") {
-    // prompted to enter the name of the department and that department is added to the database
+    dept_question = [
+      {
+        type: 'input',
+        name: 'dept_name',
+        message: 'What is the department name?',
+      },
+    ]
+    inquirer
+    .prompt(dept_question)
+    .then((answers) => {
+      let departmentName = answers.dept_name
+      db.query(`INSERT INTO department (name) VALUES (?)`, departmentName, (err, results) => {
+        if (err) {
+          console.log(err);
+        }
+        console.table(results);
+      });
+      promptUser();
+    });
   }  else if(answers.menu == "Add a role") {
     // prompted to enter the name, salary, and department for the role and that role is added to the database
+    role_questions = [
+      {
+        type: 'input',
+        name: 'role_name',
+        message: 'What is the role name?',
+      },
+      {
+        type: 'input',
+        name: 'salary',
+        message: 'What is the salary?',
+      },
+      {
+        type: 'list',
+        name: 'department',
+        message: 'What is the department?',
+        choices: [ "Dept 1", "Dept 2", "Dept 3"]
+      }
+    ]
+    inquirer
+    .prompt(role_questions)
+    .then((answers) => {
+      let title = answers.role_name
+      let salary = answers.salary
+      let department = answers.department
+      db.query(`INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`, (title, salary, department), (err, results) => {
+        if (err) {
+          console.log(err);
+        }
+        console.table(results);
+      });
+      promptUser();
+    });
   } else if(answers.menu == "Add an employee") {
     // prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
   }  else if(answers.menu == "Update an employee role") {
     // prompted to select an employee to update and their new role and this information is updated in the database
   }
   else {
-    // end application
+    return
   }
 }
 
   // Function to initialize app
 function init() {
-    inquirer
-    .prompt(question)
-    .then((answers) => {
-      checkMenuSelection(answers)
-    });
-  
+    promptUser(); 
   }
 
 // Function call to initialize app
